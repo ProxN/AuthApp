bf2c01caa98898242ba6a4eead8442c0c6440ca0
proxn <idel.ayoubkan@gmail.com>
2020-11-10 17:24:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# imup
+
+devchallenges.io full stack Challenge - Authentication App
+
+[Challenge](https://devchallenges.io/challenges/N1fvBjQfhlkctmwj1tnw#)
---
+++ client/src/styles/theme.styles.ts
@@ -0,0 +1,62 @@
+import { DefaultTheme } from 'styled-components';
+
+const fontSizes = [12, 14, 16, 18, 24, 34, 48, 58, 72];
+
+const colors = {
+  primary: {
+    light: '#71b7e6',
+    main: '#48a2df',
+    dark: '#2a7aaf',
+  },
+  secondary: {
+    light: '#f5a092',
+    main: '#f28573',
+    dark: '#d86b59',
+  },
+  danger: {
+    light: '#ee8277',
+    main: '#e95e50',
+    dark: '#d04436',
+  },
+  success: {
+    light: '#6ddb9c',
+    main: '#43d17f',
+    dark: '#29b866',
+  },
+  info: {
+    light: '#7fe6fc',
+    main: '#5adffb',
+    dark: '#41c5e2',
+  },
+  warining: {
+    light: '#e0874d',
+    main: '#d7651a',
+    dark: '#be4c00',
+  },
+  text: {
+    main: '#000',
+    secondary: '#333',
+  },
+  textInverse: {
+    main: '#fff',
+    secondary: '#BDBDBD',
+  },
+  bg: '#fff',
+  bgInverse: '#272c36',
+};
+
+const lineHeight = 1.5;
+const fontFamily = "'Nato Sans', Helvetica, sans-serif";
+const fontWeights = [400, 600, 700];
+
+const Theme: DefaultTheme = {
+  colors,
+  lineHeight,
+  fontFamily,
+  fontSizes,
+  fontWeights,
+  fontSizeBase: fontSizes[2],
+  borderRadius: '12px',
+};
+
+export default Theme;
